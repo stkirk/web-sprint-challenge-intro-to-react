1afb9aca0ec01edcc2b560309d0de0d1f7fcf5c0
stkirk <shaanekirk@gmail.com>
2021-06-26 14:13:47 -0400

    styled each component with colors, fonts, and sizes

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,21 +3,38 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledCharacter = styled.div`
-  padding: 4% 0;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+  margin: 1.5% 0;
+  padding: 2% 0;
+  border: 2px solid #2ff924;
+  border-radius: 10px;
 
   div {
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
   }
 
   h2 {
+    font-weight: bold;
     font-size: 3rem;
-    padding: 2% 0;
+    padding: 1% 0;
+    margin-left: 3%;
   }
 
   p {
+    color: whitesmoke;
     font-size: 1.8rem;
+    margin-right: 3%;
+    padding: 0 2%;
+    border: 1px solid whitesmoke;
+    border-radius: 5px;
+    background-color: #696969;
+  }
+
+  .height {
+    margin-left: 3%;
   }
 `;
 
@@ -30,7 +47,7 @@ export default function Character(props) {
         <p>Birth-Year: {characterData.birth_year}</p>
       </div>
       <div className="details-container">
-        <p>Height: {characterData.height}</p>
+        <p className="height">Height: {characterData.height}</p>
         <p>Mass: {characterData.mass}</p>
       </div>
     </StyledCharacter>
