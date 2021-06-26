a2a1f5c30f38a1a50d450d3ef31f9cfb50ad4297
stkirk <shaanekirk@gmail.com>
2021-06-26 13:14:29 -0400

    wrote out dummy data in Character component to verify its import into App

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
       <StyledCharactersSection className="characters-section">
         <Character characterData={characterData} />
       </StyledCharactersSection>
--- src/components/Character.js
+++ src/components/Character.js
@@ -5,6 +5,15 @@ import styled from "styled-components";
 const StyledCharacter = styled.div``;
 
 export default function Character(props) {
-  const { characterData } = props;
-  return <div className="character"></div>;
+  //   const { characterData } = props;
+  return (
+    <StyledCharacter className="character">
+      <h2>name</h2>
+      <p>birth_year</p>
+      <p>height</p>
+      <p>mass</p>
+      <p>homeworld</p>
+      <p>species</p>
+    </StyledCharacter>
+  );
 }
