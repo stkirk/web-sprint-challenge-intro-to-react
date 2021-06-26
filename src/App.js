da80d9f6642ef8996a6260099cdf08cdadd61b9e
stkirk <shaanekirk@gmail.com>
2021-06-26 11:51:31 -0400

    added imports to App and added a styled-component to serve as a section where the individual character components will be mapped over with some base flexbox styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import styled from "styled-components";
+
+const StyledCharacters = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <StyledCharacters className="main-section"></StyledCharacters>
     </div>
   );
-}
+};
 
 export default App;
