61d4bab67c4354b70c23bad03c46769c81258e4b
stkirk <shaanekirk@gmail.com>
2021-06-26 12:07:31 -0400

    created baseUrl variable to hold api address, instantiated characterData as state, created successful side effect for an axios GET request to baseUrl and set the response data to state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import "./App.css";
 import axios from "axios";
 import styled from "styled-components";
 
+const baseUrl = "https://swapi.dev/api/people";
+
 const StyledCharacters = styled.section`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
+  align-content: space-evenly;
 `;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterData, setCharacterData] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((res) => {
+        console.log("RES.data.", res.data);
+        setCharacterData(res.data);
+      })
+      .catch((err) => console.log("Oh no :(", err));
+  }, []);
 
   return (
     <div className="App">
